1cfdc07ce62276f53f92c81a5b50e93330b56806
Liam Watts <elnewatts@gmail.com>
2022-04-07 13:25:46 +0100

    debug code

--- src/Services/LocationHelper.php
+++ src/Services/LocationHelper.php
@@ -7,14 +7,14 @@ use Symfony\Component\HttpFoundation\Request;
 class LocationHelper {
     private Request $request;
 
-    public function __construct() 
+    public function __construct()
     {
         $this->request = Request::createFromGlobals();;
     }
 
     public function getCountryCode(): string
     {
-        $ip = $this->request->getClientIp();;
+        $ip = $this->request->getClientIp();
         dump($ip);
         $json = file_get_contents('https://www.iplocate.io/api/lookup/' . $ip);
         $ipInfo = json_decode($json);
