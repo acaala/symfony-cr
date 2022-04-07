e11e958f5a0831a81568e26b68d104592a169a94
Liam Watts <elnewatts@gmail.com>
2022-04-07 13:34:52 +0100

    Added dev variable to stop ip from breaking on local

--- src/Services/LocationHelper.php
+++ src/Services/LocationHelper.php
@@ -14,7 +14,7 @@ class LocationHelper {
 
     public function getCountryCode(): string
     {
-        $ip = $this->request->getClientIp();
+        $ip = $_ENV['APP_ENV'] == 'prod' ? $this->request->getClientIp() : '89.238.150.174';
         $json = file_get_contents('https://www.iplocate.io/api/lookup/' . $ip);
         $ipInfo = json_decode($json);
         return $ipInfo->country_code;
